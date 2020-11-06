c7e86b32175bc67d593680ea2f53541e106d23c2
Amit Kaneria <amit.kaneria@gmail.com>
2020-11-06 15:24:18 -0500

    version 0.0.6

--- hello_world/app.py
+++ hello_world/app.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
 
     #     raise e
 
-    print("Hello World, version: 0.0.5")
+    print("Hello World, version: 0.0.6")
 
     return {
         "statusCode": 200,
