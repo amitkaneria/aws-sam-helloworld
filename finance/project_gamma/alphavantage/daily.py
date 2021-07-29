f5517148548fed490b0b24e2157d849071d87566
Amit Kaneria <amit.kaneria@gmail.com>
2021-07-28 21:01:59 -0400

    running analytics by priority list

--- finance/project_gamma/alphavantage/daily.py
+++ finance/project_gamma/alphavantage/daily.py
@@ -36,9 +36,42 @@ print('##### ##### ##### ##### #####')
 if option == 1:
 
     interval = 'daily'
-    ticker_list_db = get_tickers(interval=interval, last_run_date=None, priority=None)
+    # ticker_list_db = get_tickers(interval=interval, last_run_date=None, priority=None)
     # ticker_list_db = get_tickers(last_run_date=None, priority=None)
-    print("Running Daily for List:" + str(ticker_list_db))
+    ticker_list_db = get_tickers(interval='daily', last_run_date=None, priority=1)
+    print("Priority: 1, Running Daily for List:" + str(ticker_list_db))
+    for ticker in ticker_list_db:
+        # process_data_for(ticker, api_key=API_KEY, interval='daily', date='2021-07-16')
+        # process_data_for(ticker, api_key=API_KEY, interval='weekly', date=None)
+        process_data_for(ticker, api_key=API_KEY, interval=interval, date=None)
+        print(str(datetime.datetime.now()) + ' : . . . sleeping')
+        time.sleep(25)
+
+    if len(ticker_list_db) > 0:
+        # Daily Analytics Run
+        generate_signal(interval='daily', start_date=str(previous_business_day(last_business_day(None))), end_date=None)
+    else:
+        print('NOTE: Analytics is not NOT for empty watch-list')
+    print('##### ##### ##### ##### #####')
+
+    ticker_list_db = get_tickers(interval='daily', last_run_date=None, priority=2)
+    print("Priority: 2, Running Daily for List:" + str(ticker_list_db))
+    for ticker in ticker_list_db:
+        # process_data_for(ticker, api_key=API_KEY, interval='daily', date='2021-07-16')
+        # process_data_for(ticker, api_key=API_KEY, interval='weekly', date=None)
+        process_data_for(ticker, api_key=API_KEY, interval=interval, date=None)
+        print(str(datetime.datetime.now()) + ' : . . . sleeping')
+        time.sleep(25)
+
+    if len(ticker_list_db) > 0:
+        # Daily Analytics Run
+        generate_signal(interval='daily', start_date=str(previous_business_day(last_business_day(None))), end_date=None)
+    else:
+        print('NOTE: Analytics is not NOT for empty watch-list')
+    print('##### ##### ##### ##### #####')
+
+    ticker_list_db = get_tickers(interval=interval, last_run_date=None, priority=None)
+    print("Priority: ALL, Running Daily for List:" + str(ticker_list_db))
     for ticker in ticker_list_db:
         # process_data_for(ticker, api_key=API_KEY, interval='daily', date='2021-07-16')
         # process_data_for(ticker, api_key=API_KEY, interval='weekly', date=None)
@@ -74,7 +107,6 @@ elif option == 2:
     # generate_signal(interval='daily', start_date=friday_before_previous_friday(), end_date=last_business_day())
     generate_signal(interval='daily', start_date='2020-12-31', end_date=last_business_day())
 
-
 elif option == 3:
 
     # process_data_for("UPST", api_key=API_KEY, interval='weekly', date=None)
@@ -82,18 +114,16 @@ elif option == 3:
     process_data_for(ticker='SI', api_key=API_KEY, interval='daily', date=None)
     process_data_for(ticker='MSTR', api_key=API_KEY, interval='daily', date=None)
 
-
 elif option == 4:
     # generate_signal(interval='weekly', start_date='2020-01-03', end_date='2020-12-25')
     # generate_signal(interval='daily', start_date='2020-12-31', end_date=None)
     generate_signal(interval='daily', start_date='2020-12-31', end_date='2021-07-23')
 
-
 elif option == 5:
-    ticker_list_db = get_tickers(last_run_date='2021-07-21', priority=1)
+    # ticker_list_db = get_tickers(interval='daily', last_run_date='2021-07-21', priority=1)
+    ticker_list_db = get_tickers(interval='daily', last_run_date=None, priority=9)
     print("Running for List:" + str(ticker_list_db))
 
-
 else:
-    print('Please select correct options: 1, 2, 3, 4')
+    print('Please select correct options: 1, 2, 3, 4, 5')
 
