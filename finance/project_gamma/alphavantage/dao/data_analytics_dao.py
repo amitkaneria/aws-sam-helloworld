9fa6657ffbf21dbd30fcd47e73da7c6f572ade39
Amit Kaneria <amit.kaneria@gmail.com>
2021-08-25 17:29:02 -0400

    updated to AWS database and new schemas

--- finance/project_gamma/alphavantage/dao/data_analytics_dao.py
+++ finance/project_gamma/alphavantage/dao/data_analytics_dao.py
@@ -13,9 +13,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     option = interval+"_"+buy_sell+"_"+method
 
     if option == 'daily_buy_ema.8.12':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                         SELECT A.ticker, B.date, 'buy', 'EMA_8_12', 'EMA 8 crossing EMA upside indicates buy signal, please verify with other technicals and material news, ratings changes', B.close
-                        FROM public."Daily_Data" A, public."Daily_Data" B
+                        FROM gamma.daily_data A, gamma.daily_data B
                         WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                             AND A.ema8 < A.ema12  AND B.ema8 > B.ema12
                         ON CONFLICT (ticker,date,method)
@@ -23,9 +23,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                         ;"""
 
     elif option == 'daily_sell_ema.8.12':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'sell', 'EMA_8_12', 'EMA 8 crossing EMA upside indicates buy signal, please verify with other technicals and material news, ratings changes', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                                 AND A.ema8 > A.ema12  AND B.ema8 < B.ema12
                             ON CONFLICT (ticker,date,method)
@@ -33,9 +33,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_buy_ema.21':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'buy', 'EMA_21', 'EMA 21 crossing this week price for potential buy signal, please verify with other technicals and material news, ratings changes', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                                 AND A.close < A.ema21  AND B.close > B.ema21
                             ON CONFLICT (ticker,date,method)
@@ -43,9 +43,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_sell_ema.21':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'sell', 'EMA_21', 'EMA 21 crossing this week price for potential sell signal, please verify with other technicals and material news, ratings changes', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                                 AND A.close > A.ema21  AND B.close < B.ema21
                             ON CONFLICT (ticker,date,method)
@@ -53,9 +53,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_buy_stoch.slow':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'buy', 'STOCH_SLOW', 'stoch-slow 5-3-3 indicated signal, Potential trend change. Please verify with other technicals as RSI/ EMA 21/ 8-12 crossover and material news, ratings changes', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                                 AND ((A.stochs_delta < 0 and B.stochs_delta > 0) OR ((B.stochs_delta - A.stochs_delta) > 10))
                             ON CONFLICT (ticker,date,method)
@@ -63,9 +63,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_sell_stoch.slow':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'sell', 'STOCH_SLOW', 'stoch-slow 5-3-3 indicates signal, Potential trend change. Please verify with other technicals as RSI/ EMA 21/ 8-12 crossover and material news, ratings changes', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker and A.date = %s and B.date = %s
                                 AND ((A.stochs_delta > 0 and B.stochs_delta < 0) OR ((A.stochs_delta - B.stochs_delta) > 10)) 
                             ON CONFLICT (ticker,date,method)
@@ -73,9 +73,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_buy_rsi':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'buy', 'RSI', 'RSI Oversold, please verify with other technicals and material news, ratings changes', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker and A.date = %s and B.date = %s
                                 and A.rsi < 30 and B.rsi > A.rsi 
                             ON CONFLICT (ticker,date,method)
@@ -83,9 +83,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_buy_amit.special':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'buy', 'AMIT.SP', 'Stochastic crossover with either RSI oversold, or in uptrend with price above EMA-21', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker and A.date = %s and B.date = %s
                                 and A.stochs_delta < 0 and B.stochs_delta > 0 AND ((A.rsi < 30 and B.rsi > A.rsi) OR (B.ema21 > B.close OR B.ema8 > B.ema12)) 
                             ON CONFLICT (ticker,date,method)
@@ -93,9 +93,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'daily_sell_amit.special':
-        sql = """INSERT INTO public."Daily_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.daily_signals (ticker, date, buy_sell, method, note, reco_price)
                             SELECT A.ticker, B.date, 'sell', 'AMIT.SP', 'Stochastic crossover with either RSI oversold, or in uptrend with price above EMA-21', B.close
-                            FROM public."Daily_Data" A, public."Daily_Data" B
+                            FROM gamma.daily_data A, gamma.daily_data B
                             WHERE A.ticker = B.ticker and A.date = %s and B.date = %s
                                 and A.stochs_slowk < B.stochs_slowk AND A.stochs_slowd < B.stochs_slowd 
                                 AND B.ema21 > B.close OR B.ema8 < B.ema12 
@@ -104,9 +104,9 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
                             ;"""
 
     elif option == 'weekly_buy_ema.8.12':
-        sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
+        sql = """INSERT INTO gamma.weekly_signals (ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'buy', 'EMA_8_12', 'EMA 8 crossing EMA upside indicates buy signal, please verify with other technicals and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                         AND A.ema8 < A.ema12  AND B.ema8 > B.ema12
                     ON CONFLICT (ticker,date,method)
@@ -116,7 +116,7 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     elif option == 'weekly_sell_ema.8.12':
         sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'sell', 'EMA_8_12', 'EMA 8 crossing EMA upside indicates buy signal, please verify with other technicals and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                         AND A.ema8 > A.ema12  AND B.ema8 < B.ema12
                     ON CONFLICT (ticker,date,method)
@@ -126,7 +126,7 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     elif option == 'weekly_buy_ema.21':
         sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'buy', 'EMA_21', 'EMA 21 crossing this week price for potential buy signal, please verify with other technicals and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                         AND A.close < A.ema21  AND B.close > B.ema21
                     ON CONFLICT (ticker,date,method)
@@ -136,7 +136,7 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     elif option == 'weekly_sell_ema.21':
         sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'sell', 'EMA_21', 'EMA 21 crossing this week price for potential sell signal, please verify with other technicals and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                         AND A.close > A.ema21  AND B.close < B.ema21
                     ON CONFLICT (ticker,date,method)
@@ -146,7 +146,7 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     elif option == 'weekly_buy_stoch.slow':
         sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'buy', 'STOCH_SLOW', 'stoch-slow 5-3-3 indicated signal, Potential trend change. Please verify with other technicals as RSI/ EMA 21/ 8-12 crossover and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     WHERE A.ticker = B.ticker AND A.date = %s AND B.date = %s
                         AND A.stochs_delta < 0 and B.stochs_delta > 0 
                     ON CONFLICT (ticker,date,method)
@@ -156,7 +156,7 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     elif option == 'weekly_sell_stoch.slow':
         sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'sell', 'STOCH_SLOW', 'stoch-slow 5-3-3 indicates signal, Potential trend change. Please verify with other technicals as RSI/ EMA 21/ 8-12 crossover and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     where A.ticker = B.ticker and A.date = %s and B.date = %s
                     and A.stochs_delta > 0 and B.stochs_delta < 0 
                     ON CONFLICT (ticker,date,method)
@@ -166,7 +166,7 @@ def process_signals(start_date, end_date, method, buy_sell, interval='daily'):
     elif option == 'weekly_buy_rsi':
         sql = """INSERT INTO public."Weekly_Signals"(ticker, date, buy_sell, method, note, reco_price)
                     SELECT A.ticker, B.date, 'buy', 'RSI', 'RSI Oversold, please verify with other technicals and material news, ratings changes', B.close
-                    FROM public."Weekly_Data" A, public."Weekly_Data" B
+                    FROM gamma.weekly_data A, gamma.weekly_data B
                     WHERE A.ticker = B.ticker and A.date = %s and B.date = %s
                         and A.rsi < 30 and B.rsi > A.rsi 
                     ON CONFLICT (ticker,date,method)
--- finance/project_gamma/alphavantage/query.sql
+++ finance/project_gamma/alphavantage/query.sql
@@ -1,75 +1,75 @@
-select * from public."Daily_Data" where ticker = 'SPY' order by date desc
+select * from gamma.daily_data where ticker = 'SPY' order by date desc
 
-select * from public."Daily_Data" where ticker = 'QQQ' order by date desc
+select * from gamma.daily_data where ticker = 'QQQ' order by date desc
 
-select * from public."Daily_Data" where ticker = 'TSLA' order by date desc
+select * from gamma.daily_data where ticker = 'TSLA' order by date desc
 
 select A.*
-from public."Daily_Signals" A, public."WatchList" B
+from gamma.daily_signals A, gamma.watchlist B
 where A.ticker = B.ticker and A.date >='2021-08-10' and B.priority = 0
 order by A.date desc
 
-select * from public."Daily_Signals"
+select * from gamma.daily_signals
 
---delete from public."Daily_Signals"
+--delete from gamma.daily_signals
 
---delete from public."Weekly_Signals"
+--delete from gamma.weekly_signals
 
-select * from public."Daily_Signals" where ticker = 'SHOP' order by date desc
+select * from gamma.daily_signals where ticker = 'SHOP' order by date desc
 
-select * from public."Daily_Signals" where date > '2021-07-16' order by date desc
+select * from gamma.daily_signals where date > '2021-07-16' order by date desc
 
 
-select * from public."Daily_Signals" A, public."WatchList" B
+select * from gamma.daily_signals A, gamma.watchlist B
 where A.date = '2021-07-23' and A.ticker = B.ticker and B.priority_name = 'MAIN'
 order by A.date desc
 
 
---delete from public."Daily_Data" where ticker in ('T', 'AXP', 'CARV', 'WBA')
+--delete from gamma.daily_data where ticker in ('T', 'AXP', 'CARV', 'WBA')
 
-select * from public."Daily_Data" where ticker in ('TSLA') order by date desc
+select * from gamma.daily_data where ticker in ('TSLA') order by date desc
 
-select * from public."Weekly_Data" where ticker in ('GOOGL') order by date desc
+select * from gamma."Weekly_Data" where ticker in ('GOOGL') order by date desc
 
-select * from public."Daily_Data" where ticker = 'SPY' order by date desc
-select * from public."Daily_Data" where ticker = 'SPY' order by date desc
+select * from gamma.daily_data where ticker = 'SPY' order by date desc
+select * from gamma.daily_data where ticker = 'SPY' order by date desc
 
-select * from public."Daily_Signals" where ticker = 'COIN' order by date desc
+select * from gamma.daily_signals where ticker = 'COIN' order by date desc
 
-select * from public."Daily_Signals" where date = '2021-07-07' order by date desc
+select * from gamma.daily_signals where date = '2021-07-07' order by date desc
 
---delete from public."WatchList" where ticker in ('T', 'AXP', 'CARV', 'WBA')
-select * from public."WatchList" where ticker in ('GME', 'AMC')
-select * from public."WatchList" --where priority=3
+--delete from gamma.watchlist where ticker in ('T', 'AXP', 'CARV', 'WBA')
+select * from gamma.watchlist where ticker in ('GME', 'AMC')
+select * from gamma.watchlist --where priority=3
 
-select ticker from public."WatchList" where daily_last_run_date < '2021-07-21' AND priority = 1 order by priority
+select ticker from gamma.watchlist where daily_last_run_date < '2021-07-21' AND priority = 1 order by priority
 
-select * from public."Weekly_Signals" where date = '2021-07-23'
+select * from gamma.weekly_signals where date = '2021-07-23'
 
 
-select * from public."Daily_Signals" A, public."WatchList" B
+select * from gamma.daily_signals A, gamma.watchlist B
 where A.date = '2021-07-26' and A.ticker = B.ticker and B.priority_name = 'MAIN'
 order by A.date desc
 
-select * from public."Daily_Signals" A, public."WatchList" B
+select * from gamma.daily_signals A, gamma.watchlist B
 where A.date = '2021-07-26' and A.ticker = B.ticker and B.priority_name = 'GROWTH'
 order by A.date desc
 
-select * from public."Daily_Signals" A, public."WatchList" B
+select * from gamma.daily_signals A, gamma.watchlist B
 where A.date = '2021-07-26' and A.ticker = B.ticker and B.priority_name = 'CHINA'
 order by A.date desc
 
-select * from public."Weekly_Signals" A, public."WatchList" B
+select * from gamma.weekly_signals A, gamma.watchlist B
 where A.date = '2021-07-23' and A.ticker = B.ticker and B.priority_name = 'MAIN'
 order by A.date desc
 
 --'MAR', 'WH', 'HLT', 'HGV', 'UAL', 'DAL', ''
 
-select distinct priority, priority_name from public."WatchList"
+select distinct priority, priority_name from gamma.watchlist
 order by priority
 
-select * from public."WatchList"-- where priority = 0
+select * from gamma.watchlist-- where priority = 0
 
-update public."WatchList"
+update gamma.watchlist
 set priority_name = 'MEME', priority=4 where priority =9
 
